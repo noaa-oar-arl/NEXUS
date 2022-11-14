6f239c0812cfc741239484c786ae5b843e959de2
zmoon <zmoon92@gmail.com>
2022-11-14 10:51:28 -0700

    Don't need the old vars in the new file

--- utils/combine-ant-bio.py
+++ utils/combine-ant-bio.py
@@ -79,66 +79,63 @@ def main(ifp, ofp):
     ifp, ofp : Path
         Input and output file path.
     """
-    import shutil
-
     from netCDF4 import Dataset
 
-    # Copy to ofp and open in append (edit) mode
-    shutil.copyfile(ifp, ofp)
-    ds = Dataset(ofp, "a")
+    ds = Dataset(ifp, "r")
+    ds_new = Dataset(ofp, "w")
 
     for spc in SPECIES:
 
         # 1. Use HEMCO MEGANv2.1 instantaneous diagnostic for some bio-only species
         if spc == "AACD":
-            ds[spc] = ds["InvMEGAN_AAXX"]
+            ds_new[spc] = ds["InvMEGAN_AAXX"]
         elif spc == "FACD":
-            ds[spc] = ds["InvMEGAN_FAXX"]
+            ds_new[spc] = ds["InvMEGAN_FAXX"]
         elif spc == "APIN":
-            ds[spc] = ds["InvMEGAN_APIN"]
+            ds_new[spc] = ds["InvMEGAN_APIN"]
         elif spc == "SESQ":
             # Sesquiterpene is also only biogenic
-            ds[spc] = ds["InvMEGAN_bio"]
+            ds_new[spc] = ds["InvMEGAN_bio"]
 
         # 2. The following species need to combine HEMCO anthropogenic and MEGANv2.1 biogenic species
         elif spc in {"ACET", "ALD2", "ETH", "ETOH", "ISOP", "MEOH", "OLE"}:
-            ds[spc] = ds[f"{spc}_ant"] + ds[ds[f"{spc}_bio"]]
+            ds_new[spc] = ds[f"{spc}_ant"] + ds[ds[f"{spc}_bio"]]
         elif spc == "TERP":
             # Lumped terpene should combine all HEMCO MEGANv2.1 terpenoids
-            ds[spc] = (
+            ds_new[spc] = (
                 ds[f"{spc}_ant"] + ds["MTPA_bio"] + ds["MTPO_bio"] + ds["LIMO_bio"]
             )
 
         # 3. Following species are approximately calculated from other BVOCs
         # (estimated from NACC-CMAQv5.3.1/BEISv3.6.1 summer simulation) and then combined with anthropogenic
         elif spc == "IOLE":
-            ds[spc] = ds[f"{spc}_ant"] + ds["OLE_bio"] * 0.967963
+            ds_new[spc] = ds[f"{spc}_ant"] + ds["OLE_bio"] * 0.967963
         elif spc == "PAR":
-            ds[spc] = (
+            ds_new[spc] = (
                 ds[f"{spc}_ant"]
                 + (ds["MTPA_bio"] + ds["MTBO_bio"] + ds["LIMO_bio"]) * 0.576825
             )
         elif spc == "ETHA":
-            ds[spc] = ds[f"{spc}_ant"] + ds["ETH_bio"] * 0.160406
+            ds_new[spc] = ds[f"{spc}_ant"] + ds["ETH_bio"] * 0.160406
         elif spc == "ALDX":
-            ds[spc] = ds[f"{spc}_ant"] + ds["ALD2_bio"] * 0.166038
+            ds_new[spc] = ds[f"{spc}_ant"] + ds["ALD2_bio"] * 0.166038
         elif spc == "FORM":
-            ds[spc] = ds[f"{spc}_ant"] + ds["ALD2_bio"] * 0.914909
+            ds_new[spc] = ds[f"{spc}_ant"] + ds["ALD2_bio"] * 0.914909
         elif spc == "FORM_PRIMARY":
-            ds[spc] = ds[f"{spc}_ant"] + ds["ALD2_bio"] * 0.914909
+            ds_new[spc] = ds[f"{spc}_ant"] + ds["ALD2_bio"] * 0.914909
         elif spc == "ALD2_PRIMARY":
-            ds[spc] = ds[f"{spc}_ant"] + ds["ALD2_bio"] * 0.670921
+            ds_new[spc] = ds[f"{spc}_ant"] + ds["ALD2_bio"] * 0.670921
         elif spc == "KET":
-            ds[spc] = ds[f"{spc}_ant"] + ds["ACET_bio"] * 0.0339559
+            ds_new[spc] = ds[f"{spc}_ant"] + ds["ACET_bio"] * 0.0339559
         elif spc == "CO":
-            ds[spc] = (
+            ds_new[spc] = (
                 ds[f"{spc}_ant"]
                 + (ds["MTPA_bio"] + ds["MTBO_bio"] + ds["LIMO_bio"]) * 0.4666
             )
 
         # 4. The remainder of species are just anthropogenic from HEMCO
         else:
-            ds[spc] = ds[f"{spc}_ant"]
+            ds_new[spc] = ds[f"{spc}_ant"]
 
     ds.close()
 
