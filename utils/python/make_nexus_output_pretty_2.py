b43c7edbde9bcd80bb6216c3c2cc6a44c2da0f25
zmoon <zmoon92@gmail.com>
2022-11-15 14:18:59 -0700

    fmt

--- utils/python/make_nexus_output_pretty_2.py
+++ utils/python/make_nexus_output_pretty_2.py
@@ -17,12 +17,13 @@ DEFAULT_TIME_FILE_PATH = Path("./HEMCO_sa_Time.rc")
 
 def get_hemco_dates(time_file=DEFAULT_TIME_FILE_PATH):
     import datetime as dt
+    
+    print(time_file.as_posix())
 
     def parse_dt_line(line):
         _, s_date, s_time = line.split()
         return dt.datetime.strptime(f"{s_date} {s_time}", r"%Y-%m-%d %H:%M:%S")
 
-    print(time_file.as_posix())
     start = end = ts_emis = None
     with open(time_file, "r") as f:
         for line in f:
@@ -45,7 +46,9 @@ def get_hemco_dates(time_file=DEFAULT_TIME_FILE_PATH):
 
     assert dates[0] == start, f"{dates[0]} should be same as {start}"
     assert dates[-1] == end, f"{dates[-1]} should be same as {end}"
-    assert (dates[1] - dates[0]).total_seconds() == ts_emis, f"dt {dates[1] - dates[0]} should equal {ts_emis} s"
+    assert (
+        dates[1] - dates[0]
+    ).total_seconds() == ts_emis, f"dt {dates[1] - dates[0]} should equal {ts_emis} s"
 
     return dates, start_base
 
@@ -54,19 +57,25 @@ def main(t_fp):
     dates, date_base = get_hemco_dates(t_fp)
 
 
-
 def parse_args(argv=None):
     import argparse
 
-    parser = argparse.ArgumentParser(description="Make the NEXUS output pretty",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-t", "--hemco-time-file", type=Path, default=DEFAULT_TIME_FILE_PATH,
-        help="HEMCO time file path")
+    parser = argparse.ArgumentParser(
+        description="Make the NEXUS output pretty",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "-t",
+        "--hemco-time-file",
+        type=Path,
+        default=DEFAULT_TIME_FILE_PATH,
+        help="HEMCO time file path",
+    )
 
     args = parser.parse_args(argv)
 
     return {
-         "t_fp": args.hemco_time_file,
+        "t_fp": args.hemco_time_file,
     }
 
 
