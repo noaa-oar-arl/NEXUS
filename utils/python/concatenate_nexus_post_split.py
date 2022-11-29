fe5778a0464bac8a60d140e72b9279b846e97621
zmoon <zmoon92@gmail.com>
2022-11-29 11:23:29 -0700

    cleanup

--- utils/python/concatenate_nexus_post_split.py
+++ utils/python/concatenate_nexus_post_split.py
@@ -1,64 +1,55 @@
 #!/usr/bin/env python
 """
-Concatenate all species from nexus split jobs 
+Combine output files from NEXUS split jobs into single netCDF file.
 """
-def main(ifp, ofp, *, compress=True):
+
+
+def main(ifp, ofp):
     """
     Parameters
     ----------
-    ifp, ofp : Path
+    ifp, ofp
         Input and output file path.
     """
-#    import xarray as xr
     import netCDF4 as nc4
     from glob import glob
+
     files = glob(ifp)
     files.sort()
     print(files)
 
-    #open all files
-    src = nc4.MFDataset(files, aggdim='time')
+    # Open all files
+    src = nc4.MFDataset(files, aggdim="time")
 
-    # now make new netcdf file
-    dst = nc4.Dataset(ofp,'w',format="NETCDF4")
+    # Now make new netcdf file
+    dst = nc4.Dataset(ofp, "w", format="NETCDF4")
 
-    #first create dimensions
+    # First create dimensions
     for name, dimension in src.dimensions.items():
-        dst.createDimension(name, (len(dimension) if not dimension.isunlimited() else None))
+        dst.createDimension(name, len(dimension) if not dimension.isunlimited() else None)
 
-    # now copy variables
+    # Now copy variables
     for name, variable in src.variables.items():
-        x = dst.createVariable(name, variable.dtype, variable.dimensions)
+        dst.createVariable(name, variable.dtype, variable.dimensions)
         dst[name][:] = src[name][:]
 
     dst.close()
     src.close()
-        
+
     return 0
-
-
 
 
 def parse_args(argv=None):
     import argparse
-    from pathlib import Path
-    from glob import glob
 
-    parser = argparse.ArgumentParser(
-        description="Combine outputs from NEXUS split jobs."
-    )
+    parser = argparse.ArgumentParser(description="Combine outputs from NEXUS split jobs.")
     parser.add_argument("INPUT", type=str, help="Input directory.")
     parser.add_argument("OUTPUT", type=str, help="Output file path.")
-    parser.add_argument("--compress", action="store_true", help="Whether to apply compression for the variables.")
-    parser.add_argument("--no-compress", action="store_false", dest="compress")
-    parser.set_defaults(compress=True)
 
-    
     args = parser.parse_args(argv)
     return {
         "ifp": args.INPUT,
         "ofp": args.OUTPUT,
-        "compress": args.compress,
     }
 
 
