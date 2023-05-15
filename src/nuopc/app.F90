b89af390172dec0d429c055a55ae4b6d9b2187aa
zmoon <zmoon92@gmail.com>
2023-05-15 15:11:03 -0600

    Add exe cl arg parsing from nexus_driver

--- src/nuopc/app.F90
+++ src/nuopc/app.F90
@@ -10,8 +10,33 @@ program app
 
   implicit none
 
-  integer                       :: rc, userRc
+  character(len=*), parameter :: NEXUS_options(9,2) = reshape( &
+    (/ &
+    "-c           ", "c:           ", &
+    "--config     ", "c:           ", &
+    "--config-file", "c:           ", &
+    "-r           ", "r:           ", &
+    "--regrid-to  ", "r:           ", &
+    "-d           ", "d            ", &
+    "--debug      ", "d            ", &
+    "-o           ", "o:           ", &
+    "--output     ", "o:           "  &
+    /), (/ 9, 2 /), order=(/ 2, 1 /))
+
+  integer :: rc, localrc, userRc
+  integer, parameter :: rootPet = 0
+  integer :: localPet
+  integer :: idx, ind, item
+  integer :: debugLevel
+  integer :: ibuf(2)
+  character(ESMF_MAXSTR) :: ConfigFile
+  character(ESMF_MAXSTR) :: ReGridFile
+  character(ESMF_MAXSTR) :: OutputFile
+  character(ESMF_MAXSTR) :: optarg
+  character(ESMF_MAXSTR) :: sbuf(3)
+  type(ESMF_VM) :: vm
   type(ESMF_GridComp)           :: drvComp
+
 
   ! Initialize ESMF
   call ESMF_Initialize(defaultCalkind=ESMF_CALKIND_GREGORIAN, rc=rc)
@@ -25,6 +50,90 @@ program app
     line=__LINE__, &
     file=__FILE__)) &
     call ESMF_Finalize(endflag=ESMF_END_ABORT)
+
+  ! Parse command line arguments and share information with other PETs
+  call ESMF_VMGetCurrent(vm, rc=rc)
+  if (ESMF_LogFoundError(rc, msg=ESMF_LOGERR_PASSTHRU, &
+    line=__LINE__,  &
+    file=__FILE__)) &
+    call ESMF_Finalize(rc=rc, endflag=ESMF_END_ABORT)
+
+  call ESMF_VMGet(vm, localPet=localPet, rc=rc)
+  if (ESMF_LogFoundError(rc, msg=ESMF_LOGERR_PASSTHRU, &
+    line=__LINE__,  &
+    file=__FILE__)) &
+    call ESMF_Finalize(rc=rc, endflag=ESMF_END_ABORT)
+
+  ConfigFile = ""
+  ReGridFile = ""
+  OutputFile = ""
+
+  debugLevel = 0
+
+  localrc = ESMF_SUCCESS
+
+  if (localPet == 0) then
+    do item = 1, size(NEXUS_options, dim=1)
+      call ESMF_UtilGetArgIndex(NEXUS_options(item,1), argindex=ind, rc=localrc)
+      if (ESMF_LogFoundError(localrc, msg=ESMF_LOGERR_PASSTHRU, &
+        line=__LINE__,  &
+        file=__FILE__)) &
+        exit
+      if (ind > -1) then
+        idx = len_trim(NEXUS_options(item,2))
+        if (NEXUS_options(item,2)(idx:idx) == ":") then
+          call ESMF_UtilGetArg(ind+1, argvalue=optarg, rc=localrc)
+          if (ESMF_LogFoundError(localrc, msg=ESMF_LOGERR_PASSTHRU, &
+            line=__LINE__,  &
+            file=__FILE__)) &
+            exit
+        end if
+        select case (trim(NEXUS_options(item,2)))
+         case ("c:")
+          ConfigFile = optarg
+         case ("r:")
+          ReGridFile = optarg
+         case ("o:")
+          OutputFile = optarg
+         case ("d")
+          debugLevel = 1
+         case ("h")
+         case default
+        end select
+      end if
+    end do
+  end if
+
+  print "('ConfigFile = ', a)", trim(ConfigFile)
+  print "('ReGridFile = ', a)", trim(ReGridFile)
+  print "('debugLevel = ', i0)", debugLevel
+  print "('OutputFile = ', a)", trim(OutputFile)
+
+  ibuf(1) = localrc
+  ibuf(2) = debugLevel
+  call ESMF_VMBroadcast(vm, ibuf, size(ibuf), rootPet, rc=rc)
+  if (ESMF_LogFoundError(rc, msg=ESMF_LOGERR_PASSTHRU, &
+    line=__LINE__,  &
+    file=__FILE__)) &
+    call ESMF_Finalize(rc=rc, endflag=ESMF_END_ABORT)
+  localrc    = ibuf(1)
+  debugLevel = ibuf(2)
+  if (ESMF_LogFoundError(localrc, msg="Failure retrieving command-line arguments", &
+    line=__LINE__,  &
+    file=__FILE__)) &
+    call ESMF_Finalize(rc=rc, endflag=ESMF_END_ABORT)
+
+  sbuf(1) = ConfigFile
+  sbuf(2) = ReGridFile
+  sbuf(3) = OutputFile
+  call ESMF_VMBroadcast(vm, sbuf, size(sbuf)*len(sbuf(1)), rootPet, rc=rc)
+  if (ESMF_LogFoundError(rc, msg=ESMF_LOGERR_PASSTHRU, &
+    line=__LINE__,  &
+    file=__FILE__)) &
+    call ESMF_Finalize(rc=rc, endflag=ESMF_END_ABORT)
+  ConfigFile = sbuf(1)
+  ReGridFile = sbuf(2)
+  OutputFile = sbuf(3)
 
   !-----------------------------------------------------------------------------
 
--- src/nuopc/driver.F90
+++ src/nuopc/driver.F90
@@ -1,130 +1,130 @@
 module nexus_driver
 
-    !-----------------------------------------------------------------------------
-    ! Code that specializes generic NUOPC_Driver
-    !-----------------------------------------------------------------------------
-  
-    use ESMF
-    use NUOPC
-    use NUOPC_Driver, driverSS => SetServices
-  
-    use nexus_cap, only: modelSS => SetServices
-  
-    implicit none
-  
-    private
-  
-    ! private module data --> ONLY PARAMETERS
-    integer, parameter            :: stepCount = 5
-    real(ESMF_KIND_R8), parameter :: stepTime  = 30._ESMF_KIND_R8  ! step time [s]
-                                                        ! should be parent step
-  
-    public SetServices
-  
-    !-----------------------------------------------------------------------------
-    contains
-    !-----------------------------------------------------------------------------
-  
-    subroutine SetServices(driver, rc)
-      type(ESMF_GridComp)  :: driver
-      integer, intent(out) :: rc
-  
-      rc = ESMF_SUCCESS
+  !-----------------------------------------------------------------------------
+  ! Code that specializes generic NUOPC_Driver
+  !-----------------------------------------------------------------------------
 
-      ! Add custom variable
-      ! Example fd here: https://github.com/ESCOMP/NUOPCFieldDictionary/blob/master/fd.yaml
-      call NUOPC_FieldDictionaryAddEntry("the_best_variable_of_all", "1", rc=rc)
-      if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-        line=__LINE__, &
-        file=__FILE__)) &
-        return  ! bail out
-  
-      ! derive from NUOPC_Driver
-      call NUOPC_CompDerive(driver, driverSS, rc=rc)
-      if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-        line=__LINE__, &
-        file=__FILE__)) &
-        return  ! bail out
-  
-      ! specialize driver
-      call NUOPC_CompSpecialize(driver, specLabel=label_SetModelServices, &
-        specRoutine=SetModelServices, rc=rc)
-      if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-        line=__LINE__, &
-        file=__FILE__)) &
-        return  ! bail out
-  
-      ! set driver verbosity
-      call NUOPC_CompAttributeSet(driver, name="Verbosity", value="high", rc=rc)
-      if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-        line=__LINE__, &
-        file=__FILE__)) &
-        return  ! bail out
-  
-    end subroutine
-  
-    !-----------------------------------------------------------------------------
-  
-    subroutine SetModelServices(driver, rc)
-      type(ESMF_GridComp)  :: driver
-      integer, intent(out) :: rc
-  
-      ! local variables
-      type(ESMF_GridComp)           :: child
-      type(ESMF_CplComp)            :: connector
-      type(ESMF_Time)               :: startTime
-      type(ESMF_Time)               :: stopTime
-      type(ESMF_TimeInterval)       :: timeStep
-      type(ESMF_Clock)              :: internalClock
-  
-      rc = ESMF_SUCCESS
-  
-      ! SetServices for MODEL component
-      call NUOPC_DriverAddComp(driver, "MODEL", modelSS, comp=child, rc=rc)
-      if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-        line=__LINE__, &
-        file=__FILE__)) &
-        return  ! bail out
-      call NUOPC_CompAttributeSet(child, name="Verbosity", value="low", rc=rc)
-      if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-        line=__LINE__, &
-        file=__FILE__)) &
-        return  ! bail out
-  
-      ! set the driver clock
-      call ESMF_TimeSet(startTime, s = 0, rc=rc)
-      if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-        line=__LINE__, &
-        file=__FILE__)) &
-        call ESMF_Finalize(endflag=ESMF_END_ABORT)
-  
-      call ESMF_TimeSet(stopTime, s_r8 = stepTime * stepCount, rc=rc)
-      if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-        line=__LINE__, &
-        file=__FILE__)) &
-        call ESMF_Finalize(endflag=ESMF_END_ABORT)
-  
-      call ESMF_TimeIntervalSet(timeStep, s_r8 = stepTime, rc=rc)
-      if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-        line=__LINE__, &
-        file=__FILE__)) &
-        call ESMF_Finalize(endflag=ESMF_END_ABORT)
-  
-      internalClock = ESMF_ClockCreate(name="Driver Clock", &
-        timeStep=timeStep, startTime=startTime, stopTime=stopTime, rc=rc)
-      if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-        line=__LINE__, &
-        file=__FILE__)) &
-        call ESMF_Finalize(endflag=ESMF_END_ABORT)
-  
-      call ESMF_GridCompSet(driver, clock=internalClock, rc=rc)
-      if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-        line=__LINE__, &
-        file=__FILE__)) &
-        return  ! bail out
-  
-    end subroutine
-  
-    !-----------------------------------------------------------------------------
-  
-  end module nexus_driver
+  use ESMF
+  use NUOPC
+  use NUOPC_Driver, driverSS => SetServices
+
+  use nexus_cap, only: modelSS => SetServices
+
+  implicit none
+
+  private
+
+  ! private module data --> ONLY PARAMETERS
+  integer, parameter            :: stepCount = 5
+  real(ESMF_KIND_R8), parameter :: stepTime  = 30._ESMF_KIND_R8  ! step time [s]
+  ! should be parent step
+
+  public SetServices
+
+  !-----------------------------------------------------------------------------
+contains
+  !-----------------------------------------------------------------------------
+
+  subroutine SetServices(driver, rc)
+    type(ESMF_GridComp)  :: driver
+    integer, intent(out) :: rc
+
+    rc = ESMF_SUCCESS
+
+    ! Add custom variable
+    ! Example fd here: https://github.com/ESCOMP/NUOPCFieldDictionary/blob/master/fd.yaml
+    call NUOPC_FieldDictionaryAddEntry("the_best_variable_of_all", "1", rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    ! derive from NUOPC_Driver
+    call NUOPC_CompDerive(driver, driverSS, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    ! specialize driver
+    call NUOPC_CompSpecialize(driver, specLabel=label_SetModelServices, &
+      specRoutine=SetModelServices, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    ! set driver verbosity
+    call NUOPC_CompAttributeSet(driver, name="Verbosity", value="high", rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+  end subroutine
+
+  !-----------------------------------------------------------------------------
+
+  subroutine SetModelServices(driver, rc)
+    type(ESMF_GridComp)  :: driver
+    integer, intent(out) :: rc
+
+    ! local variables
+    type(ESMF_GridComp)           :: child
+    type(ESMF_CplComp)            :: connector
+    type(ESMF_Time)               :: startTime
+    type(ESMF_Time)               :: stopTime
+    type(ESMF_TimeInterval)       :: timeStep
+    type(ESMF_Clock)              :: internalClock
+
+    rc = ESMF_SUCCESS
+
+    ! SetServices for MODEL component
+    call NUOPC_DriverAddComp(driver, "MODEL", modelSS, comp=child, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    call NUOPC_CompAttributeSet(child, name="Verbosity", value="low", rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    ! set the driver clock
+    call ESMF_TimeSet(startTime, s = 0, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      call ESMF_Finalize(endflag=ESMF_END_ABORT)
+
+    call ESMF_TimeSet(stopTime, s_r8 = stepTime * stepCount, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      call ESMF_Finalize(endflag=ESMF_END_ABORT)
+
+    call ESMF_TimeIntervalSet(timeStep, s_r8 = stepTime, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      call ESMF_Finalize(endflag=ESMF_END_ABORT)
+
+    internalClock = ESMF_ClockCreate(name="Driver Clock", &
+      timeStep=timeStep, startTime=startTime, stopTime=stopTime, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      call ESMF_Finalize(endflag=ESMF_END_ABORT)
+
+    call ESMF_GridCompSet(driver, clock=internalClock, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+  end subroutine
+
+  !-----------------------------------------------------------------------------
+
+end module nexus_driver
