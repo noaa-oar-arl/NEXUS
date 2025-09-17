5a3d13319a5b404be0b1f64f7b64fa8b2a99cd50
bbakernoaa <barry.baker@noaa.gov>
2025-09-17 13:31:05 +0000

    updating modulefiles to mimic the latest ufswm

--- modulefiles/ufs_container.intel.lua
+++ modulefiles/ufs_container.intel.lua
@@ -1,21 +1,20 @@
 help([[
-loads UFS Model prerequisites for Hera/Intel
+Load environment to compile ufs-weather-model in a container using Intel
 ]])
 
-prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/intel-oneapi-mpi/2021.13-sbi3u54/gcc/13.3.0")
+prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.9.2/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.9.2/envs/unified-env/install/modulefiles/intel-oneapi-mpi/2021.13-argr3sd/gcc/11.4.0")
 
-stack_intel_ver=os.getenv("stack_intel_ver") or "2024.2.1"
-load(pathJoin("stack-oneapi", stack_intel_ver))
-
+stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.0"
 stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
+
+load(pathJoin("stack-oneapi", stack_oneapi_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 
 cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 load(pathJoin("cmake", cmake_ver))
 
 load("ufs_common")
-load("zlib/1.2.11")
 
 nccmp_ver=os.getenv("nccmp_ver") or "1.9.0.1"
 load(pathJoin("nccmp", nccmp_ver))
@@ -27,6 +26,6 @@ setenv("I_MPI_CC", "icx")
 setenv("I_MPI_CXX", "icpx")
 setenv("I_MPI_F90", "ifort")
 
-setenv("CMAKE_Platform", "hera.intel")
+setenv("CMAKE_Platform", "container.intel")
 
 whatis("Description: UFS build environment")
